f749eee6a69a398c1f26bb283e282cc05ba207a3
Temz cruise <sosanyaayonitemi@gmail.com>
2020-08-12 12:40:32 +0100

    Database models creation

---
+++ database/migrations/2020_08_12_100328_create_offers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('location');
+            $table->datetime('bid_start_date');
+            $table->datetime('bid_end_date');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
